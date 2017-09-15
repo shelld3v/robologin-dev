345ecfcef212d8efdec632a448ecb42006e650c2
maurosoria <AguanteGithubPERONCARAJO1337>
2017-09-15 04:45:09 -0300

    MVC

--- lib/model/wordlist/PlainTextWordlist.py
+++
@@ -1,8 +0,0 @@
-class PlainTextWordlist(BaseWordlist):
-	def __init__(self, fields=["username", "password"], separator=":"):
-		self.fields = fields
-		self.separator = separator
-
-	def parse_content(self, path, default_values=None):
-		pass
-
